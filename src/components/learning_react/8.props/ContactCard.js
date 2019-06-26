33766e9510d832a174e014032ca90878858387c9
Ben Hagag <benbenhagag@google.com>
2019-06-26 22:25:18 +0300

    fix syntax add alt property to image becasue jsx makes us to

--- src/components/learning_react/8.props/ContactCard.js
+++ src/components/learning_react/8.props/ContactCard.js
@@ -13,7 +13,7 @@ function ContactCard(props) {
 
   return (
     <div className="contact-card">
-      <img src={props.imgUrl} />
+      <img src={props.imgUrl} alt="ben" />
       <h3>{props.name}</h3>
       <p>{props.phone}</p>
       <p>{props.email}</p>
