c71345440b9e0b6836bd8ae48556cb632154e041
Ben Hagag <benbenhagag@google.com>
2019-08-10 13:33:31 +0300

    add in comment the mission - to get rid of the warning about no having on change

--- src/components/learning_react/17.todoAppPhase5/TodoAppPhase5.js
+++ src/components/learning_react/17.todoAppPhase5/TodoAppPhase5.js
@@ -2,6 +2,10 @@ import React from "react";
 import todoData from "./todoData";
 import TodoAppPhase5Render from "./TodoAppPhase5Render";
 
+/**
+ * Challenge: Get rid of our warning about not having an onChange on our input. For now, the function that runs onChange can simply console.log something.
+ */
+
 class TodoAppPhase5 extends React.Component {
   constructor() {
     super();
