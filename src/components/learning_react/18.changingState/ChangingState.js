30a0445a45077eed753a6b9b602c025d0da883ad
Ben Hagag <benbenhagag@google.com>
2019-08-10 14:41:32 +0300

    change state by on click and binding

--- src/components/learning_react/18.changingState/ChangingState.js
+++ src/components/learning_react/18.changingState/ChangingState.js
@@ -4,13 +4,27 @@ class ChangingState extends React.Component {
   constructor() {
     super();
     this.state = { count: 0 };
+    // this.onClick = this.changeState.bind(this);
+    // this.onClick = this.changeState.bind(this);
+  }
+
+  changeState() {
+    let count = this.state.count;
+    this.setState({ count: count + 1 });
+  }
+
+  restartState() {
+    this.setState({ count: 0 });
   }
 
   render() {
     return (
       <div>
         <h1>{this.state.count}</h1>
-        <button>Change State!</button>
+        <button count={this.state.count} onClick={() => this.changeState()}>
+          Change State!
+        </button>
+        <button onClick={() => this.restartState()}> Restart State </button>
       </div>
     );
   }
